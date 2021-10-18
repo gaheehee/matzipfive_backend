a2bf155a48b9730903e5d9870c1366f2c079ab1b
Jeong Ga Hee <jghgahee12@gmail.com>
2021-10-18 14:10:23 +0900

    l

--- src/test/java/com/example/firstproject/repository/UserRepositoryTest.java
+++ src/test/java/com/example/firstproject/repository/UserRepositoryTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
