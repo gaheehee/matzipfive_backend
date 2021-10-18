0f87b651e0d5128275d436277432358bc15da5d0
Jeong Ga Hee <jghgahee12@gmail.com>
2021-10-18 15:31:59 +0900

    j

--- src/test/java/com/example/firstproject/repository/UserRepositoryTest.java
+++ src/test/java/com/example/firstproject/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(value = "dev")
 public class UserRepositoryTest {
 
     @Autowired
